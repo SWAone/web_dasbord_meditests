5d77a1a662466bfbb78a3aa019e7e3e5034880a9
SWAone <abass.fadeil1100@gmail.com>
2025-01-16 15:01:18 +0300

    aj

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Hono } from 'hono'
+import { createBunWebSocket, serveStatic } from 'hono/bun'
+
+const app = new Hono();
+app.get('/*', serveStatic({
+  root: "/web/", rewriteRequestPath: p => {
+    // console.log(p)
+    return p.replace(/^\/web/, '')
+  }
+}))
+
+app.get('/admin/*', serveStatic({
+  root: "/web/", rewriteRequestPath: p => {
+
+    return p.replace(/^\/admin/, '')
+  }
+}))
+app.get('/tajer*', serveStatic({
+  root: "/tajer", rewriteRequestPath: p => {
+
+    return p.replace(/^\/tajer/, '')
+  }
+}))
+app.get('/', serveStatic({ path: "/web/index.html" }))
+app.get('/admin', serveStatic({ path: "/web/index.html" }))
+app.get('/tajer*', serveStatic({ path: "/tajer/index.html" }))
+
+
+
+export default {
+  fetch: app.fetch,
+  port: 3600
+}
